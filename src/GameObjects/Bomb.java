d60c1dc62a7987f602e814b3338cbe6e7ea659ee
Khoa <nguyenanhkhoablue@gmail.com>
2020-10-26 11:09:14 +0700

    Add new Images and clean up code

--- src/GameObjects/Bomb.java
+++ src/GameObjects/Bomb.java
@@ -7,10 +7,6 @@ import java.awt.Rectangle;
 import javax.swing.ImageIcon;
 
 public class Bomb extends Actor{
-//    protected int x,y,type,orient, speed, width, height, runBomb;
-//	protected Image img;
-//	protected int x,y,size,widthBomb, heightBomb,timeline,type;
-//	protected Image img;
     protected int size, timeline;
 
     public Bomb(int x, int y, int size, int timeline){
@@ -69,21 +65,5 @@ public class Bomb extends Actor{
         Rectangle rec1 = new Rectangle(x, y, 45, 45);
         Rectangle rec2 = new Rectangle(xNewBomb, yNewBomb, 45, 45);
         return rec1.intersects(rec2);
-    }
-
-
-    public int isImpactBombvsActor(Actor actor){
-        if(actor.getRunBomb()==Bomber.ALLOW_RUN) {
-            return 0;
-        }
-        Rectangle rec2 = new Rectangle(x, y, 45, 45);
-        Rectangle rec3 = new Rectangle(actor.getX(), actor.getY(), actor.getWidth(), actor.getHeight());
-        if(rec2.intersects(rec3)){
-            if(actor.getType()==Bomber.BOSS){
-                return 2;
-            }
-            return 1;
-        };
-        return 0;
     }
 }
