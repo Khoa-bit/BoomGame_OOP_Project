ce95fd86ba3212a0ade79803a0c39b6f53652c17
Dev03-vinamr <73330185+Dev03-vinamr@users.noreply.github.com>
2020-12-01 11:36:41 +0700

    test push

---
+++ src/GameObjects/Item.java
@@ -0,0 +1,9 @@
+package GameObjects;
+
+public class Item {
+    // test the git commit :<<<<
+    public static int Item_Bomb = 1;
+    public static int Item_BombSize = 2;
+    public static int Item_Shoe = 3;
+    private int x, y, type, width, height, timeline;
+}
