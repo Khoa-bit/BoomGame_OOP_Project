48cf13a2ad4890cff61f1d989c7614ba7411610e
Dev03-vinamr <73330185+Dev03-vinamr@users.noreply.github.com>
2020-12-01 11:46:39 +0700

    Update Item.java

--- src/GameObjects/Item.java
+++ src/GameObjects/Item.java
@@ -6,4 +6,17 @@ public class Item {
     public static int Item_BombSize = 2;
     public static int Item_Shoe = 3;
     private int x, y, type, width, height, timeline;
+
+    // Getter and Setter
+    public int getX() {
+        return x;
+    }
+
+    public int getY() {
+        return y;
+    }
+
+    public int getType() {
+        return type;
+    }
 }
