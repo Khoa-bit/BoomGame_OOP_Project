a135a88d96832c7184c593612ce8a19a0d647bb1
Mai Oanh <63551651+maioanh2602@users.noreply.github.com>
2020-12-06 21:31:48 +0700

    fix something

--- src/com/GUI/Menu.java
+++ src/com/GUI/Menu.java
@@ -102,8 +102,7 @@ public class Menu extends JPanel{
                 mGui.dispose();
                 PlayGame.IS_RUNNING=false;
             }
-            if(e.getSource()==lbHigthScore){
-				mContainer.setShowHightScore();
-        }
-    };
+		}
+	};
+	
 }
