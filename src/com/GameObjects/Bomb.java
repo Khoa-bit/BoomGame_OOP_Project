f5aa2f7e67fe72f722e18c867d23babeb4a847df
HTD2k1 <superdatlocpro@gmail.com>
2021-01-05 00:48:41 +0700

    Using new class Character in Bomb.java

--- src/com/GameObjects/Bomb.java
+++ src/com/GameObjects/Bomb.java
@@ -6,39 +6,39 @@ import java.awt.Rectangle;
 
 import javax.swing.ImageIcon;
 
-public class Bomb extends Actor{
+public class Bomb extends Character {
     protected int size, timeline;
 
-    public Bomb(int x, int y, int size, int timeline){
-        x=(x/ GUI.TILES)*GUI.TILES;
-        y=(y/GUI.TILES)*GUI.TILES;
-        this.x=x;
-        this.y=y;
-        this.size=size;
-        this.orient=0;
+    public Bomb(int x, int y, int size, int timeline) {
+        x = (x / GUI.TILES) * GUI.TILES;
+        y = (y / GUI.TILES) * GUI.TILES;
+        this.x = x;
+        this.y = y;
+        this.size = size;
+        this.orient = 0;
         this.timeline = timeline;
-        this.type =Actor.BOMB;
-        img= new ImageIcon(getClass().getResource("/Images/boom.png")).getImage();
-        this.width= img.getWidth(null);
-        this.height= img.getHeight(null);
-    }
-
-    public Bomb(int x, int y,int orient, int speed, int size, int timeline){
-        x=(x/GUI.TILES)*GUI.TILES;
-        y=(y/GUI.TILES)*GUI.TILES;
-        this.x=x;
-        this.y=y;
-        this.orient = orient;
-        this.speed = 5;
-        this.size=size;
-        this.timeline = timeline;
-        this.type =Actor.BOMB;
-        img= new ImageIcon(getClass().getResource("/Images/boom.png")).getImage();
+        this.type = Character.BOMB;
+        img = new ImageIcon(getClass().getResource("/Images/boom.png")).getImage();
         this.width = img.getWidth(null);
         this.height = img.getHeight(null);
     }
 
-    public void deadlineBomb(){
+    public Bomb(int x, int y, int orient, int speed, int size, int timeline) {
+        x = (x / GUI.TILES) * GUI.TILES;
+        y = (y / GUI.TILES) * GUI.TILES;
+        this.x = x;
+        this.y = y;
+        this.orient = orient;
+        this.speed = 5;
+        this.size = size;
+        this.timeline = timeline;
+        this.type = Character.BOMB;
+        img = new ImageIcon(getClass().getResource("/Images/boom.png")).getImage();
+        this.width = img.getWidth(null);
+        this.height = img.getHeight(null);
+    }
+
+    public void deadlineBomb() {
         this.timeline--;
     }
 
@@ -50,7 +50,7 @@ public class Bomb extends Actor{
         return size;
     }
 
-    public boolean setRun(Bomber bomber){
+    public boolean setRun(Bomber bomber) {
         Rectangle rec2 = new Rectangle(x, y, GUI.TILES, GUI.TILES);
         Rectangle rec3 = new Rectangle(bomber.getX(), bomber.getY(), bomber.getWidth(), bomber.getHeight());
         return rec2.intersects(rec3);
@@ -60,20 +60,19 @@ public class Bomb extends Actor{
         this.timeline = timeline;
     }
 
-
-    public boolean isImpact(int xNewBomb, int yNewBomb){
+    public boolean isImpact(int xNewBomb, int yNewBomb) {
         Rectangle rec1 = new Rectangle(x, y, GUI.TILES, GUI.TILES);
         Rectangle rec2 = new Rectangle(xNewBomb, yNewBomb, GUI.TILES, GUI.TILES);
         return rec1.intersects(rec2);
     }
 
-    public int isImpactBombvsActor(Actor actor){
-        if(actor.getRunBomb()==Bomber.ALLOW_RUN) {
+    public int doesBombImpactCharacter(Character character) {
+        if (character.getRunBomb() == Bomber.ALLOW_RUN) {
             return 0;
         }
         Rectangle rec2 = new Rectangle(x, y, 45, 45);
-        Rectangle rec3 = new Rectangle(actor.getX(), actor.getY(), actor.getWidth(), actor.getHeight());
-        if(rec2.intersects(rec3)){
+        Rectangle rec3 = new Rectangle(character.getX(), character.getY(), character.getWidth(), character.getHeight());
+        if (rec2.intersects(rec3)) {
             return 1;
         }
         return 0;
